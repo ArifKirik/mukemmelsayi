7ce0c5ca22c1a6d45cf586c399ac7fc86594ae13
kocen <kocen@ARIF_KIRIK>
2024-11-21 00:11:50 +0300

    Proje dosyası ekle.

---
+++ 221120241014 Arif Kirik Mükemmel sayı/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _221120241014_Arif_Kirik_Mükemmel_sayı
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { int sayi,bölentoplam;
+            bölentoplam = 0;
+            Console.WriteLine("bir sayı giriniz:");
+            sayi=Convert.ToInt32(Console.ReadLine());
+
+            for(int i = sayi; i >0; i/=10)
+            {
+                bölentoplam =+ i % 10;
+            }
+            if(bölentoplam==sayi)
+            {
+                Console.WriteLine("sayi mükemmel sayıdır");
+            }
+            else
+            {
+                Console.WriteLine("sayi mükemmel değildir");
+            }
+
+        }
+    }
+}
